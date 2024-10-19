ad1d322710f1f610ab78ec843c61de2dd7ad65f3
NullPointerExcy <danjelp.galic@gmail.com>
2024-10-19 20:22:30 +0200

    Minor changes.

--- SmartPDFManager/utils.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        first_page = reader.pages[0]
-        text = first_page.extract_text()
-        return text.strip()
