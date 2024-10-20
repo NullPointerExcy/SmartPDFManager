c38134b59eb1abe93a31d88992b8cf67b9bca627
NullPointerExcy <danjelp.galic@gmail.com>
2024-10-20 14:04:45 +0200

    Updated to pypdf v5.0.1, because of possible Infinite Loop on pypdf2

--- smart_pdf_manager/utils.py
+++ smart_pdf_manager/utils.py
@@ -1,9 +1,9 @@
-import PyPDF2
+import pypdf
 
 
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
+        reader = pypdf.PdfReader(file)
         first_page = reader.pages[0]
         text = first_page.extract_text()
         return text.strip()
